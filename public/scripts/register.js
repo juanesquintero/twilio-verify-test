9f175d0dca9090aae1c56c8e27d2f112fc119f1c
juanquintero <juanestquintero@gmail.com>
2023-11-10 19:23:54 -0500

    feat(ui): change action if 2fa on register form

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -6,25 +6,41 @@ const register = async (event) => {
   const phone = document.getElementById('phone').value;
   const twoFA = document.getElementById('2fa').checked;
 
-  try {
-    const response = await fetch('/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-
-      body: JSON.stringify({ name, password, phone, twoFA }),
-    });
-
-    window.location = '/';
-  } catch (error) {
-    document.getElementById('error').style.display = 'block';
+  if (!twoFA) {
+    apiRegister({ name, password, phone, twoFA })
   }
 };
 
-document.getElementById('register-form').addEventListener('submit', register);
+const apiRegister = async (user) => {
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+  
+        body: JSON.stringify(user),
+      });
+  
+      window.location = '/';
+    } catch (error) {
+      document.getElementById('error').style.display = 'block';
+    }
+}
+
+var registerForm = document.getElementById('register-form');
+ 
+registerForm.addEventListener('submit', register);
 
 document.getElementById('2fa').addEventListener('change', function () {
   var phoneInput = document.getElementById('phone');
   phoneInput.required = this.checked;
+  if (this.checked) {
+    registerForm.addEventListener('submit', function (event) {
+      this.submit();
+    });
+    registerForm.action = 'verify/register'
+  } else {
+    registerForm.addEventListener('submit', register);
+  }
 });
